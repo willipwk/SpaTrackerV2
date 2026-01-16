e72fbe392d648acc3d508374e94d7a1301509d46
willipwk <p_w_kun@163.com>
2026-01-15 20:02:11 -0800

    make a package for spatrackerv2

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name="SpaTrack2",
+    version="0.0.1",
+    author="Yuxi Xiao",
+    url="",
+    packages=setuptools.find_packages(),
+    license="",
+    python_requires=">3.7",
+    # install_requires=[
+    # 'altered_midas @ git+https://github.com/CCareaga/MiDaS@master',
+    # 'chrislib @ git+https://github.com/CCareaga/chrislib@main'
+    # ]
+)
